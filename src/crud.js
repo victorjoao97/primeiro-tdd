f047bfe10a531efee8a339fdb147d648a561c6ea
João Victor Nascimento <joao.victor@salesoft.com.br>
2022-04-22 23:31:57 -0300

    criar teste crud

---
+++ src/crud.js
@@ -0,0 +1,23 @@
+export default class {
+    constructor(serviceDA) {
+        if (!serviceDA) throw Error('ServiceDA não foi injetado')
+        this.serviceDA = serviceDA
+    }
+    insert(user) {
+        if (this.findByEmail(user.email))
+            throw Error('Não inserir registro duplicado')
+        this.serviceDA.insert(user)
+    }
+    find(id) {
+        return this.serviceDA.find(id)
+    }
+    findByEmail(email) {
+        return this.serviceDA.where(r => r.email === email)[0] || null
+    }
+    update(user) {
+        return this.serviceDA.update(user)
+    }
+    delete(id) {
+        return this.serviceDA.delete(id)
+    }
+}
