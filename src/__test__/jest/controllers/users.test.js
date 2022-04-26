3967adce80da0a00b362d50a28aee6dbb13ca7d4
João Victor Nascimento <joao.victor@salesoft.com.br>
2022-04-26 00:34:34 -0300

    criar testes  user controller find

--- src/__test__/jest/controllers/users.test.js
+++ src/__test__/jest/controllers/users.test.js
@@ -1,18 +1,28 @@
 import UserBusiness from "../../../business/users";
-import Users from "../../../controllers/users"
+import UsersController from "../../../controllers/users"
 import User from "../../../models/user";
 import DA from '../../../services/da'
 
 describe('Testes para a controller de Users', () => {
     test('mostrar erro se não for passado business', () => {
-        expect(() => new Users()).toThrow('UserBusiness não foi injetado')
+        expect(() => new UsersController()).toThrow('UserBusiness não foi injetado')
     });
     test('criação de controller com sucesso', () => {
-        expect(() => new Users(new UserBusiness(new DA()))).not.toThrowError()
+        expect(() => new UsersController(new UserBusiness(new DA()))).not.toThrowError()
     });
     test('adicionar usuario pela controller', () => {
-        const controller = new Users(new UserBusiness(new DA([])))
+        const controller = new UsersController(new UserBusiness(new DA([])))
         const userData = new User({name: 'Pan', email: 'i@pan.com'})
-        expect(controller.addUser(userData)).toEqual(Object.assign({}, userData, {id: 1}))
+        expect(controller.add(userData)).toEqual(Object.assign({}, userData, {id: 1}))
+    });
+    test('buscar um usuario que existe', () => {
+        const userValid = {id: 1, name: 'Pan', email: 'i@pan.com'}
+        const controller = new UsersController(new UserBusiness(new DA([userValid])))
+        expect(controller.findId(1)).toEqual(userValid)
+    });
+    test('buscar um usuario que nao existe', () => {
+        const userValid = {id: 1, name: 'Pan', email: 'i@pan.com'}
+        const controller = new UsersController(new UserBusiness(new DA([userValid])))
+        expect(controller.findId(2)).toEqual(null)
     });
 });
