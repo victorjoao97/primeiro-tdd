632c0f3d61844a5c2728fc68a3ee8b91de8546ae
João Victor Nascimento <dev.joao.vn@outlook.com>
2022-04-30 17:12:22 -0300

    refac: start do server

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import app from './services/app'
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log(`Server running http://localhost:${PORT}`))
+app().listen(PORT, () => console.log(`Server running http://localhost:${PORT}`))
